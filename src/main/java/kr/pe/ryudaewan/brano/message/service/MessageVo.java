0d6ec35915fc9b844e453ada20dd2bd791668b3c
ryudaewan <ryudaewan@gmail.com>
2025-12-19 13:47:32 +0900

    equals(), hashCode() 재정의

--- src/main/java/kr/pe/ryudaewan/brano/message/service/MessageVo.java
+++ src/main/java/kr/pe/ryudaewan/brano/message/service/MessageVo.java
@@ -5,6 +5,8 @@ import kr.pe.ryudaewan.brano.base.service.CommonVo;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Getter
 @Setter
 public class MessageVo extends CommonVo {
@@ -15,4 +17,17 @@ public class MessageVo extends CommonVo {
     private String messageKey;
     @NotBlank
     private String messageContent;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MessageVo messageVo = (MessageVo) obj;
+
+        return Objects.equals(locale, messageVo.locale) && Objects.equals(messageKey, messageVo.messageKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(locale, messageKey);
+    }
 }
