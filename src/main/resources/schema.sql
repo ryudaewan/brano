34c62721bf0fb52bfda663c277f3f68b503a65cd
ryudaewan <ryudaewan@gmail.com>
2025-12-30 18:08:23 +0900

    (구현 중) 불투명 토큰 + 스프링 시큐리티

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,8 +5,11 @@ create table users
     email      varchar(255) not null Unique,
     password   varchar(1000),
     created_at timestamp    not null,
+    created_by varchar(255),
     updated_at timestamp,
-    deleted_at timestamp
+    updated_by varchar(255),
+    deleted_at timestamp,
+    deleted_by varchar(255)
 );
 
 CREATE TABLE messages
@@ -15,8 +18,11 @@ CREATE TABLE messages
     locale          VARCHAR(100)  NOT NULL,
     message_key     VARCHAR(255)  NOT NULL,
     message_content varchar(1000) NOT NULL,
-    created_at      timestamp     not null,
-    updated_at      timestamp,
-    deleted_at      timestamp,
+    created_at timestamp    not null,
+    created_by varchar(255),
+    updated_at timestamp,
+    updated_by varchar(255),
+    deleted_at timestamp,
+    deleted_by varchar(255)
     CONSTRAINT unique_locale_key UNIQUE (locale, message_key)
 );
